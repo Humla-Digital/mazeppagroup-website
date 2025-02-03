ee49abb392068e2d7e97454de1bd743fafbff20d
Jonas Linnestad <137791671+humlajonas@users.noreply.github.com>
2025-02-03 16:19:43 +0100

    fix lenis editor

--- src/global.ts
+++ src/global.ts
@@ -5,17 +5,24 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Lenis from 'lenis';
 
+const lenis = new Lenis();
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
+
+declare global {
+  interface Window {
+    WebflowEditor: unknown;
+  }
+}
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const lenis = new Lenis();
-
-  lenis.on('scroll', ScrollTrigger.update);
-
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 1000);
-  });
-
-  gsap.ticker.lagSmoothing(0);
+  //Run in both editor and live
   let isOpened = false;
   function update() {
     if (isOpened) {
@@ -33,4 +40,10 @@ window.Webflow.push(() => {
 
   $('.home-article-item .is-portrait').on('click', onClick);
   $('.portrait-interview-close').on('click', onClick);
+  if (!window.WebflowEditor) {
+    // Run in live only
+  } else {
+    //Run in editor only
+    lenis.destroy();
+  }
 });
